986975d616cdd65a9d1cea5c1cbd3307484d0d6c
root <root@localhost.localdomain>
2023-04-18 13:17:36 +0530

    devbranch

---
+++ dev1/index.js
@@ -0,0 +1,13 @@
+kjadf,j
+jgaksjb.as
+bkasjbc,asmn.a,mnas.
+],,kajsb,asa			
+kjaddhc
+	kandlvnadv.dm.da
+		,adncckan.dada.ad,c
+		,ndc,nad,dnv,dmnd,vmd,dam
+	,jadnvlkadnvljdnvdavnda,v
+,adjv,adnv,dvnd,vmd,vnm		
+		m svsd.kvmsdkvmd.vmd\
+\
+	,sjfv,sdvn,lkj,admnf,kad,m
